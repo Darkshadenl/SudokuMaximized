006e7f550a6dc2d304621f743fe749ab2797094d
Quinten <q.m.b@hotmail.com>
2022-05-05 12:25:07 +0200

    Basis

---
+++ SudokuTesting/UnitTest1.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SudokuTesting;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
