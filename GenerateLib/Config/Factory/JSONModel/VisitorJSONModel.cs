776b8307adc15dc0f614df19b5f3ae3721f19b95
Они Лам <onyilam@hotmail.com>
2022-05-31 15:12:13 +0200

    git deleted my files :( - put back config files

---
+++ GenerateLib/Config/Factory/JSONModel/VisitorJSONModel.cs
@@ -0,0 +1,13 @@
+﻿namespace GenerateLib.Config.Factory.JSONModel;
+
+public class VisitorJSONModel
+{
+    public Visitor[] visitor { get; set; }
+}
+
+public class Visitor
+{
+    public string match { get; set; }
+    public string library { get; set; }
+    public string _namespace { get; set; }
+}
