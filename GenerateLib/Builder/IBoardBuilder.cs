832e2ea21b5bbf953829fb9e792acba1ec8ebf2c
Они Лам <onyilam@hotmail.com>
2022-06-06 17:24:09 +0200

    removed unused props

--- GenerateLib/Builder/IBoardBuilder.cs
+++ GenerateLib/Builder/IBoardBuilder.cs
@@ -8,10 +8,6 @@ public interface IBoardBuilder
     public IBoardBuilder Reset();
 
     public IBoardConfig SetBoardFile(BoardFile boardFile);
-    // public IBoardBuilder SetRows(int i);
-    // public IBoardBuilder SetCols(int i);
-    // public IBoardBuilder SetSquares(int i);
-    // public IBoardBuilder SetType(string type);
-    // public IBoardBuilder SetSquareLength(int i);
+
     public AbstractBoard Build();
 }
